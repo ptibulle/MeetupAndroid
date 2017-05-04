05554fdc344b35d0c85076156d5ea72dc84d8e32
Pierre Tibulle <pierretibulle@MacBook-Pro-de-Pierre.local>
2017-05-04 16:34:42 +0200

    suppression app cordova

--- meetupApp/platforms/android/assets/www/cordova_plugins.js
+++ meetupApp/platforms/android/assets/www/cordova_plugins.js
@@ -9,19 +9,19 @@ module.exports = [
         ]
     },
     {
-        "id": "fr.ptibulle.cordova.meetupservice1.meetupservice1",
-        "file": "plugins/fr.ptibulle.cordova.meetupservice1/www/meetupservice1.js",
-        "pluginId": "fr.ptibulle.cordova.meetupservice1",
-        "clobbers": [
-            "meetupservice1"
-        ]
-    },
-    {
         "id": "fr.ptibulle.cordova.meetupservice3.meetupservice3",
         "file": "plugins/fr.ptibulle.cordova.meetupservice3/www/meetupservice3.js",
         "pluginId": "fr.ptibulle.cordova.meetupservice3",
         "clobbers": [
             "meetupservice3"
+        ]
+    },
+    {
+        "id": "fr.ptibulle.cordova.meetupservice1.meetupservice1",
+        "file": "plugins/fr.ptibulle.cordova.meetupservice1/www/meetupservice1.js",
+        "pluginId": "fr.ptibulle.cordova.meetupservice1",
+        "clobbers": [
+            "meetupservice1"
         ]
     }
 ];
@@ -30,8 +30,8 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.3.2",
     "fr.ptibulle.cordova.meetupservice2": "1.0.0",
-    "fr.ptibulle.cordova.meetupservice1": "1.0.0",
-    "fr.ptibulle.cordova.meetupservice3": "1.0.0"
+    "fr.ptibulle.cordova.meetupservice3": "1.0.0",
+    "fr.ptibulle.cordova.meetupservice1": "1.0.0"
 };
 // BOTTOM OF METADATA
 });
